bde8ef5d857eca1b184750bc9e35cc1ff630a1bd
Jacinta Calixte <jacinta246@yahoo.com>
2020-03-20 23:18:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,6 +14,6 @@ def add_element_to_end_of_array(array, element)
 end
 
 #add_element_to_start_of_array
-def add_element_to_end_of_array(array, element)
+def add_element_to_start_of_array(array, element)
  array.unshift("element")
 end

e0406020f159a1c66b57b851cd0c757d312a69d4
Jacinta Calixte <jacinta246@yahoo.com>
2020-03-20 22:18:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,16 @@
+#create_an_empty_array
 def create_an_empty_array
-  
+  []
 end
 
+#create_an_array
 def create_an_array
-  
+  [1,2,3,4]
 end
 
+#add_element_to_end_of_array
 def add_element_to_end_of_array(array, element)
-  
+  ["pizza", "pasta", "cheese"]
 end
 
-def add_element_to_start_of_array(array, element)
-  
-end
-
-def remove_element_from_end_of_array(array)
-  
-end
-
-def remove_element_from_start_of_array(array)
-  
-end
-
-def retrieve_element_from_index(array, index_number)
-  
-end
-
-def retrieve_first_element_from_array(array)
-  
-end
-
-def retrieve_last_element_from_array(array)
-  
-end
+add_element_to_end_of_array(array, element) << "rice"
